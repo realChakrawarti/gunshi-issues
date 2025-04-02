27145cb6744bf6e87aa5706fc2c60d6cc88392b3
realChakrawarti <anupam.chakrawarti@outlook.com>
2025-04-02 17:32:25 +0530

    bug: description clashes

--- main.ts
+++ main.ts
@@ -1,3 +1,33 @@
+import { ArgOptions, cli, Command } from "@kazupon/gunshi";
+
+export const command: Command<ArgOptions> = {
+  name: "test",
+  description: "Create a test",
+  options: {
+    title: {
+      type: "string",
+      short: "t",
+      required: true,
+      default: "",
+      description: "Test title",
+    },
+    description: {
+      type: "string",
+      short: "d",
+      description: "Test description [Optional]",
+    },
+  },
+  run: (ctx) => {
+    const { title, description } = ctx.values;
+    if (title) {
+      console.log("Title is: ", title);
+      console.log("Description is: ", description);
+    } else {
+      console.log("Title is required");
+    }
+  },
+};
+
 if (import.meta.main) {
-  console.log("Gunshi");
+  cli(Deno.args, command);
 }
